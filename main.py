bde45706acbd872cbc80dcf09dbb3b8a1ef2ab11
sam <sementinchurin@gmail.com>
2023-06-04 16:06:15 +0300

    rapidapi search

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from googleapiclient.discovery import build
-from config import GOOGLE_KEY
+import requests
+from config import CUSTOM_API_KEY
 
+# https://rapidapi.com/contextualwebsearch/api/web-search/
 
-def search_google():
-    res = build('SearchEngine', developerKey=GOOGLE_KEY).cse()
-    return res.list()
+url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI"
 
+querystring = {"q": "taylor swift", "pageNumber": "1", "pageSize": "10", "autoCorrect": "true"}
 
-print(search_google())
+headers = {
+    "X-RapidAPI-Key": CUSTOM_API_KEY,
+    "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
