84c70c7ec30675955077b6bb3b9f3d7993f05028
sam <sementinchurin@gmail.com>
2023-06-10 11:56:35 +0300

    separate file for functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+from functools import wraps
+import datetime
+
+
+def timer(func):
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        def wrapper(*args, **kwargs):
+            start_time = datetime.datetime.now()
+            result = func(*args, **kwargs)
+            end_time = datetime.datetime.now()
+            delta = (end_time - start_time).total_seconds()
+            functions_work_time[func.__name__] = delta
+            return result
+        return wrapper
+    return decorator(func)
