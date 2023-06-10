07cf2c625aacd34667e62462e3840e29f3d170a1
sam <sementinchurin@gmail.com>
2023-06-10 15:05:10 +0300

    serpstack/google search function

--- search_all.py
+++ search_all.py
@@ -1,7 +1,8 @@
 import requests
-from config import RAPIDAPI_HEADERS
+from config import RAPIDAPI_HEADERS, SERPSTACK_KEY
 
 
+# https://rapidapi.com/contextualwebsearch/api/web-search/
 def rapid_search(QUERY):
     url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI"
     querystring = {"q": QUERY, "pageNumber": "1", "pageSize": "10", "autoCorrect": "true"}
@@ -15,3 +16,10 @@ def rapid_search(QUERY):
 
 
 rapid_search(QUERY='metallica')
+
+
+# https://serpstack.com/dashboard
+def serpstack_search(QUERY):
+    url = f'http://api.serpstack.com/search?access_key={SERPSTACK_KEY}&query={QUERY}'
+    response = requests.get(url).json()
+    print(response)
